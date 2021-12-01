766a55266e039bee5146e15da7b13cf5341adb15
markosbahgat <markos.bahgat@gmail.com>
2021-12-01 17:59:47 +0200

    first commit

---
+++ __test__/checkForNameTest.spec.js
@@ -0,0 +1,5 @@
+import { checkForName } from "../src/client/js/nameChecker.js"
+describe("Testing the submit functionality", () => {
+    test("Testing the checkForName() function", () => {
+           expect(checkForName).toBeDefined();
+})});
